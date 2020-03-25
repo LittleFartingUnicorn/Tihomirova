7ba8c59f61825fe6992287eaddc9e358f15c2eb2
LittleFartingUnicorn <jominat@mail.ru>
2020-03-25 19:03:53 +0300

    comment

--- scheme.sql
+++ scheme.sql
@@ -1,43 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.7.7
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1:3306
--- Время создания: Мар 25 2020 г., 18:36
--- Версия сервера: 5.6.38
--- Версия PHP: 5.5.38
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- База данных: `yeticave`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `categories`
---
 
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
   `title` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Структура таблицы `lots`
---
 
 CREATE TABLE `lots` (
   `id_of_lot` int(11) NOT NULL,
@@ -53,11 +20,9 @@ CREATE TABLE `lots` (
   `id_category` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Структура таблицы `rate`
---
+
+
 
 CREATE TABLE `rate` (
   `rate_id` int(11) NOT NULL,
@@ -67,11 +32,9 @@ CREATE TABLE `rate` (
   `id_of_lot` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Структура таблицы `user`
---
+
+
 
 CREATE TABLE `user` (
   `rate_id` int(11) NOT NULL,
@@ -84,20 +47,14 @@ CREATE TABLE `user` (
   `contacts` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Индексы сохранённых таблиц
---
 
---
--- Индексы таблицы `categories`
---
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`);
 
---
--- Индексы таблицы `lots`
---
+
+
+
 ALTER TABLE `lots`
   ADD PRIMARY KEY (`id_of_lot`),
   ADD UNIQUE KEY `id_of_lot` (`id_of_lot`),
@@ -108,9 +65,7 @@ ALTER TABLE `lots`
   ADD KEY `id_category` (`id_category`),
   ADD KEY `date_of_completion` (`date_of_completion`);
 
---
--- Индексы таблицы `rate`
---
+
 ALTER TABLE `rate`
   ADD PRIMARY KEY (`rate_id`),
   ADD UNIQUE KEY `rate_id` (`rate_id`),
@@ -118,62 +73,40 @@ ALTER TABLE `rate`
   ADD KEY `user_id` (`user_id`),
   ADD KEY `id_of_lot` (`id_of_lot`);
 
---
--- Индексы таблицы `user`
---
+
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `user_id` (`user_id`),
   ADD KEY `email` (`email`);
 
---
--- AUTO_INCREMENT для сохранённых таблиц
---
 
---
--- AUTO_INCREMENT для таблицы `categories`
---
+
+
 ALTER TABLE `categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT для таблицы `lots`
---
+
 ALTER TABLE `lots`
   MODIFY `id_of_lot` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT для таблицы `rate`
---
+
 ALTER TABLE `rate`
   MODIFY `rate_id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT для таблицы `user`
---
+
 ALTER TABLE `user`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Ограничения внешнего ключа сохраненных таблиц
---
 
---
--- Ограничения внешнего ключа таблицы `lots`
---
+
+
 ALTER TABLE `lots`
   ADD CONSTRAINT `lots_ibfk_1` FOREIGN KEY (`id_of_seller`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `lots_ibfk_2` FOREIGN KEY (`id_of_winner`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `lots_ibfk_3` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Ограничения внешнего ключа таблицы `rate`
---
 ALTER TABLE `rate`
   ADD CONSTRAINT `rate_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `rate_ibfk_2` FOREIGN KEY (`id_of_lot`) REFERENCES `lots` (`id_of_lot`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
